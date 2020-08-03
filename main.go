2c2ce986be57d51433404cb9059423df72871aa3
Balint Csergo <death0wl@fb.com>
2020-08-03 14:01:04 +0100

    draw our "character circle" in the middle of camerapos Vector. Place it on "sort of" the starting pont.

--- main.go
+++ main.go
@@ -2,20 +2,19 @@ package main
 
 import (
 	"fmt"
-	"image/png"
 	"math"
-	"os"
 	"time"
 
 	"golang.org/x/image/colornames"
 
 	"github.com/deathowl/rpg/world"
 	"github.com/faiface/pixel"
+	"github.com/faiface/pixel/imdraw"
 	"github.com/faiface/pixel/pixelgl"
 	"github.com/lafriks/go-tiled"
 )
 
-var clearColor = colornames.Skyblue
+var clearColor = colornames.Black
 
 var (
 	frames = 0
@@ -27,12 +26,17 @@ func gameloop(win *pixelgl.Window, tilemap *tiled.Map, renderedBg pixel.Picture,
 
 	var (
 		camPos       = *initialPos
-		camSpeed     = 1000.0
+		camSpeed     = 50.0
 		camZoom      = 4.0
 		camZoomSpeed = 1.2
 	)
 
 	last := time.Now()
+	rsprirte := pixel.NewSprite(renderedBg, renderedBg.Bounds())
+	mat := pixel.IM
+	mat = mat.Moved(win.Bounds().Center())
+	mat = mat.ScaledXY(win.Bounds().Center(), pixel.V(win.Bounds().Size().X/renderedBg.Bounds().Size().X, win.Bounds().Size().Y/renderedBg.Bounds().Size().Y))
+	fmt.Println(mat)
 	for !win.Closed() {
 		dt := time.Since(last).Seconds()
 		last = time.Now()
@@ -41,16 +45,26 @@ func gameloop(win *pixelgl.Window, tilemap *tiled.Map, renderedBg pixel.Picture,
 		cam := pixel.IM.Scaled(camPos, camZoom).Moved(win.Bounds().Center().Sub(camPos))
 		win.SetMatrix(cam)
 		if win.Pressed(pixelgl.KeyLeft) {
-			camPos.X -= camSpeed * dt
+			if camPos.X > camSpeed*dt {
+				camPos.X -= camSpeed * dt
+			} else {
+				fmt.Println(camPos.X)
+			}
 		}
 		if win.Pressed(pixelgl.KeyRight) {
-			camPos.X += camSpeed * dt
+			if camPos.X < win.Bounds().Size().X-camSpeed*dt {
+				camPos.X += camSpeed * dt
+			}
 		}
 		if win.Pressed(pixelgl.KeyDown) {
 			camPos.Y -= camSpeed * dt
 		}
 		if win.Pressed(pixelgl.KeyUp) {
 			camPos.Y += camSpeed * dt
+		}
+		if win.Pressed(pixelgl.KeySpace) {
+			fmt.Println(camPos.X)
+			fmt.Println(camPos.Y)
 		}
 		camZoom *= math.Pow(camZoomSpeed, win.MouseScroll().Y)
 
@@ -60,10 +74,11 @@ func gameloop(win *pixelgl.Window, tilemap *tiled.Map, renderedBg pixel.Picture,
 		for _, batch := range batches {
 			batch.Clear()
 		}
-
-		rsprirte := pixel.NewSprite(renderedBg, renderedBg.Bounds())
-		mat := pixel.IM
 		rsprirte.Draw(win, mat)
+		imd := imdraw.New(nil)
+		imd.Push(camPos)
+		imd.Circle(3.0, 2.0)
+		imd.Draw(win)
 		frames++
 		select {
 		case <-second:
@@ -77,6 +92,7 @@ func gameloop(win *pixelgl.Window, tilemap *tiled.Map, renderedBg pixel.Picture,
 }
 
 func initialize() {
+	fmt.Println("initialize called")
 	// Create the window with OpenGL
 	cfg := pixelgl.WindowConfig{
 		Title:  "Tiled Rpg",
@@ -91,31 +107,27 @@ func initialize() {
 	tilemap := world.LoadTileMap("./island.tmx")
 	fmt.Println(tilemap)
 	renderedBg := world.RenderTilemap(&tilemap)
-	startPos := &pixel.ZV
+
+	startPos := win.Bounds().Center()
+	scalingFacX := win.Bounds().Size().X / renderedBg.Bounds().Size().X
+	scalingFacY := renderedBg.Bounds().Size().Y / win.Bounds().Size().Y
 	for _, ob := range tilemap.ObjectGroups[0].Objects {
 		fmt.Println(ob.Name)
 		if ob.Type == "start" {
-			startPos = &pixel.Vec{X: ob.X / 2, Y: ob.Y}
+			fmt.Println(ob.X)
+			fmt.Println(scalingFacX)
+			fmt.Println(ob.Y)
+			fmt.Println(scalingFacY)
+			startPos = pixel.Vec{X: ob.X * scalingFacX, Y: ob.Y * scalingFacY}
+			fmt.Println(startPos.X)
+			fmt.Println(startPos.Y)
 		}
 		fmt.Printf("%+v\n", ob)
 	}
 
-	panicIfErr(err)
-
 	fmt.Println("use WASD to move camera around")
-	gameloop(win, &tilemap, renderedBg, startPos)
-}
+	gameloop(win, &tilemap, renderedBg, &startPos)
 
-func loadSprite(path string) (*pixel.Sprite, *pixel.PictureData) {
-	fmt.Println(path)
-	f, err := os.Open(path)
-	panicIfErr(err)
-
-	img, err := png.Decode(f)
-	panicIfErr(err)
-
-	pd := pixel.PictureDataFromImage(img)
-	return pixel.NewSprite(pd, pd.Bounds()), pd
 }
 
 func main() {
